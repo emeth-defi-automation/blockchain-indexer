36999155ae25610d532d3aef89afe7490491e99d
Uacias <uaciasdev@gmail.com>
2024-05-11 19:46:22 +0200

    test crates

---
+++ crates/axum-server/src/server.rs
@@ -0,0 +1,41 @@
+use axum::{http::StatusCode, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+
+pub async fn start() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let app: Router = Router::new().route("/", post(create_user));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-mod models;
 mod database;
+mod models;
 mod networking;
 mod utils;
 
+use axum_server::server::start;
+use crate::models::errors::ServerError;
+use crate::models::wallet::Wallet;
+use crate::utils::get_balance_history::get_balance_history;
 use networking::get_block_request::get_block_request;
 use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, Surreal};
-use crate::models::wallet::Wallet;
-use crate::models::errors::ServerError;
-use crate::utils::get_balance_history::get_balance_history;
 
 #[tokio::main]
 async fn main() -> Result<(), ServerError> {
@@ -15,10 +16,10 @@ async fn main() -> Result<(), ServerError> {
     db.signin(Root {
         username: "root",
         password: "root",
-    }).await?;
+    })
+    .await?;
     let chain = "sepolia".to_string();
     let to_block = get_block_request(&chain).await?;
     get_balance_history(&chain, to_block).await?;
     Ok(())
 }
-
