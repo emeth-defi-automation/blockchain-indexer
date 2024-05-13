3b5f3c16597438c0680afdabd3c97e48b9ee60bc
Uacias <uaciasdev@gmail.com>
2024-05-13 09:43:43 +0200

    create crates, fix code

--- crates/jp/src/main.rs
+++ crates/jp/src/main.rs
@@ -3,15 +3,21 @@ mod models;
 mod networking;
 mod utils;
 
-use axum_server::server::start;
 use crate::models::errors::ServerError;
 use crate::models::wallet::Wallet;
 use crate::utils::get_balance_history::get_balance_history;
+use axum_server::server::start;
 use networking::get_block_request::get_block_request;
 use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, Surreal};
 
 #[tokio::main]
 async fn main() -> Result<(), ServerError> {
+    // initialize tracing
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+    start().await;
+
     let db = Surreal::new::<Ws>("localhost:8000").await?;
     db.signin(Root {
         username: "root",
